c51c76ed5fc47d8ab1fbf1446e27d9f5fce01de8
Isabella Henriques Szrajbman <68662472+isabellahenriques@users.noreply.github.com>
2020-09-01 13:27:21 -0300

    Valores únicos em uma lista

---
+++ ex079.py
@@ -0,0 +1,19 @@
+'''Crie um programa onde o usuário possa digitar vários valores numéricos e
+cadastre-os em uma lista.
+Caso o número já exista lá dentro, ele não será adicionado.
+No final, serão exibidos todos os valores únicos digitados, em ordem crescente'''
+numero = list()
+while True:
+    n = int(input("Digite um valor: "))
+    if n not in numero:
+        numero.append(n)
+        print("Valor adicionado!")
+    else:
+        print("Valor duplicado! Não adicionado!")
+    resposta = str(input("Quer continuar? [S/N] ")).upper().strip()
+    if resposta in "N":
+        break
+print("=+" *30)
+numero.sort()
+print(f'Você digitou os valor {numero}')
+
