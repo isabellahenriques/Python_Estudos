7a3fd3890ee9f773c4a074cffce8c78f0fd773ef
Isabella Henriques Szrajbman <68662472+isabellahenriques@users.noreply.github.com>
2020-09-01 13:18:29 -0300

    analisador de dados

---
+++ ex056.py
@@ -0,0 +1,26 @@
+'''Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas.
+No final do programa, mostre: a média de idade do grupo,
+qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.'''
+somaIdade = 0
+mediaIdade = 0
+maiorIdadeHomem = 0
+nomeVelho = ''
+totMulher20 = 0
+for p in range(1,5):
+    print("----- {}ª PESSOA -----".format(p))
+    nome = str(input("Qual o seu nome: ")).strip()
+    idade = int(input("Qual a sua idade: "))
+    sexo = str(input("Sexo [F/M]: ")).strip()
+    somaIdade = somaIdade + idade
+    if p == 1 and sexo in 'Mm':
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in 'Mm' and idade > maiorIdadeHomem:
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in "Ff" and idade < 20:
+        totMulher20 += 1
+mediaIdade = somaIdade / 4
+print("A média de idade do grupo é de {} anos.".format(mediaIdade))
+print("O homem mais velho tem {} anos e se chama {}". format(maiorIdadeHomem,nomeVelho))
+print("Ao todo são {} mulheres com menos de 20 anos.".format(totMulher20))
