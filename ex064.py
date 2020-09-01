6b5762556fbec23a9b2eb9ecd889d7e6116e57a1
Isabella Henriques Szrajbman <68662472+isabellahenriques@users.noreply.github.com>
2020-09-01 13:21:03 -0300

    Tratando vários valores

---
+++ ex064.py
@@ -0,0 +1,12 @@
+'''Crie um programa que leia vários números inteiros pelo teclado.
+O programa só vai parar quando o usuário digitar o valor 999,
+que é a condição de parada. No final, mostre quantos números foram digitados
+e qual foi a soma entre eles (desconsiderando o flag).'''
+numero = cont = soma = 0
+numero = int(input("Digite um número [999 para parar]: "))
+while numero != 999:
+    soma = soma + numero
+    cont = cont + 1
+    numero = int(input("Digite um número [999 para parar]: "))
+print("Você digitou {} números e a soma ente eles foi {}.".format(cont,soma1))
+
