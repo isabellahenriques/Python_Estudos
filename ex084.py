fe1566861f8f976e8921aa289ffd1d6188a9b097
Isabella Henriques Szrajbman <68662472+isabellahenriques@users.noreply.github.com>
2020-09-01 13:28:47 -0300

    listas compostas e análise de dados

---
+++ ex084.py
@@ -0,0 +1,39 @@
+'''Faça um programa que leia nome e peso de várias pessoas,
+guardando tudo em uma lista. No final, mostre:
+A) Quantas pessoas foram cadastradas.
+B) Uma listagem com as pessoas mais pesadas.
+C) Uma listagem com as pessoas mais leves.'''
+
+
+temporaria = []
+principal = []
+maiorPeso = menorPeso = 0
+while True:
+    temporaria.append(str(input("Nome: ")))
+    temporaria.append(float(input("Peso: ")))
+    if len(principal) == 0:
+        maiorPeso = menorPeso = temporaria[1]
+    else:
+        if temporaria[1] > maiorPeso:
+            maiorPeso = temporaria[1]
+        if temporaria[1] < menorPeso:
+            menorPeso = temporaria[1]
+    principal.append(temporaria[:])
+    temporaria.clear()
+    resposta = str(input("Quer continuar? [S/N] ")).upper()
+    if resposta in 'N':
+        break
+print("-=" * 30)
+print(f'Os dados foram {principal}')
+print(f'Ao todo você cadastrou {len(principal)} pessoas.')
+print(f'O maior peso foi de {maiorPeso} kg. Peso de ', end="")
+for p in principal:
+    if p[1] == maiorPeso:
+        print(f'[{p[0]}] ', end="")
+print()
+print(f'O menor peso foi de {menorPeso} kg. Peso de ', end="")
+for p in principal:
+    if p[1] == menorPeso:
+        print(f'[{p[0]}] ', end="")
+print()
+
