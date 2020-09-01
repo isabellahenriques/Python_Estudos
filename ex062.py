57425b1489239f60ea6ffde1a08c96a830c55559
Isabella Henriques Szrajbman <68662472+isabellahenriques@users.noreply.github.com>
2020-09-01 13:20:22 -0300

    PA v3.0

---
+++ ex062.py
@@ -0,0 +1,18 @@
+'''Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
+O programa encerrará quando ele disser que quer mostrar 0 termos.'''
+print("Gerador de PA")
+primeiroTermo = int(input("Qual o primeiro Termo da PA: "))
+razao = int(input("Qual a razão dessa PA: "))
+termo = primeiroTermo
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print("{} ->".format(termo), end=" ")
+        termo = termo + razao
+        cont = cont + 1
+    print("Pausa")
+    mais = int(input("Quantos termos você quer mostrar a mais? "))
+print("Progressão finalizada com {} termos mostrados".format(total))
