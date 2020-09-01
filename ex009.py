9632046c43639a4d0ea1dba245efcbe230160361
Isabella Henriques Szrajbman <68662472+isabellahenriques@users.noreply.github.com>
2020-09-01 10:32:25 -0300

    Tabuada

---
+++ ex009.py
@@ -0,0 +1,21 @@
+#Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+n = int(input("Digite um número para ver sua tabuada: "))
+t1 = n * 1
+t2 = n * 2
+t3 = n * 3
+t4 = n * 4
+t5 = n * 5
+t6 = n * 6
+t7 = n * 7
+t8 = n * 8
+t9 = n * 9
+
+print(n,"x 1 =",t1)
+print(n,"x 2 =",t2)
+print(n,"x 3 =",t3)
+print(n,"x 4 =",t4)
+print(n,"x 5 =",t5)
+print(n,"x 6 =",t6)
+print(n,"x 7 =",t7)
+print(n,"x 8 =",t8)
+print(n,"x 9 =",t9)
